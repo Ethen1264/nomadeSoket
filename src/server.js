ae0fe51260b082499668aaa78ac032887352308e
정태관 <jxorhks86909763@gmail.com>
2024-04-19 00:40:45 +0900

    add #2.1 Installing SocketIO

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import http from 'http';
-import WebSocket from 'ws';
+import SocketIO from 'socket.io';
 import express from 'express';
-import { type } from 'os';
 
 const app = express();
 
@@ -13,29 +12,30 @@ app.get('/*', (_, res) => res.redirect('/'));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-function onSocketClose() {
-  console.log('Disconnected from the Browser ❌');
-}
-
-const sockets = [];
-
-wss.on('connection', (socket) => {
-  sockets.push(socket);
-  socket['nickname'] = 'Anon';
-  console.log('Connected to Browser ✅');
-  socket.on('close', onSocketClose);
-  socket.on('message', (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case 'new_message':
-        sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload} `));
-      case 'nickname':
-        socket['nickname'] = message.payload;
-    }
-  });
+wsServer.on('connection', (socket) => {
+  console.log(socket);
 });
 
-server.listen(3000, handleListen);
+// const wss = new WebSocket.Server({ server });
+// const sockets = [];
+
+// wss.on('connection', (socket) => {
+//   sockets.push(socket);
+//   socket['nickname'] = 'Anon';
+//   console.log('Connected to Browser ✅');
+//   socket.on('close', onSocketClose);
+//   socket.on('message', (msg) => {
+//     const message = JSON.parse(msg);
+//     switch (message.type) {
+//       case 'new_message':
+//         sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload} `));
+//       case 'nickname':
+//         socket['nickname'] = message.payload;
+//     }
+//   });
+// });
+
+httpServer.listen(3000, handleListen);
