97066144322b5956698bcceef022082e809e764b
정태관 <jxorhks86909763@gmail.com>
2024-04-23 00:37:06 +0900

    add #2.3 Recap

--- src/server.js
+++ src/server.js
@@ -14,32 +14,34 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on('connection', (socket) => {
-  socket.on('enter_room', (msg, done) => {
-    console.log(msg);
+  socket.on('enter_room', (roomName, done) => {
+    console.log(roomName);
     setTimeout(() => {
-      done();
-    }, 10000);
+      done('hello from the backend');
+    }, 15000);
   });
 });
 
-// const wss = new WebSocket.Server({ server });
-// const sockets = [];
-
-// wss.on('connection', (socket) => {
-//   sockets.push(socket);
-//   socket['nickname'] = 'Anon';
-//   console.log('Connected to Browser ✅');
-//   socket.on('close', onSocketClose);
-//   socket.on('message', (msg) => {
-//     const message = JSON.parse(msg);
-//     switch (message.type) {
-//       case 'new_message':
-//         sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload} `));
-//       case 'nickname':
-//         socket['nickname'] = message.payload;
-//     }
-//   });
-// });
+/*
+const wss = new WebSocket.Server({ server });
+const sockets = [];
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
+  console.log("Connected to Browser ✅");
+  socket.on("close", onSocketClose);
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
+  });
+}); */
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
