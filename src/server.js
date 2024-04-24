5227861c061b4e705661a3fdf870f25ae9d6007e
정태관 <jxorhks86909763@gmail.com>
2024-04-24 15:57:31 +0900

    add #2.6 Room Notifications

--- src/server.js
+++ src/server.js
@@ -18,9 +18,17 @@ wsServer.on('connection', (socket) => {
     console.log(`Socket Event: ${event}`);
   });
   socket.on('enter_room', (roomName, done) => {
+    console.log(roomName);
     socket.join(roomName);
     done();
     socket.to(roomName).emit('welcome');
+  });
+  socket.on('disconnecting', () => {
+    socket.rooms.forEach((room) => socket.to(room).emit('bye'));
+  });
+  socket.on('new_message', (msg, room, done) => {
+    socket.to(room).emit('new_message', msg);
+    done();
   });
 });
 
