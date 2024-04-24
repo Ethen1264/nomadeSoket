0361ddf97f31e4952c3129da58e8f65d09675e1b
정태관 <jxorhks86909763@gmail.com>
2024-04-24 21:08:32 +0900

    add #2.9 Room Count part Two

--- src/server.js
+++ src/server.js
@@ -20,11 +20,11 @@ function publicRooms() {
     },
   } = wsServer;
   const publicRooms = [];
-  rooms.forEach((_, key)) => {
-    if(sids.get(key) === undefined) {
-      publicRooms.push(key)
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
     }
-  }
+  });
   return publicRooms;
 }
 
@@ -37,9 +37,13 @@ wsServer.on('connection', (socket) => {
     socket.join(roomName);
     done();
     socket.to(roomName).emit('welcome', socket.nickname);
+    wsServer.sockets.emit('room_change', publicRooms());
   });
   socket.on('disconnecting', () => {
     socket.rooms.forEach((room) => socket.to(room).emit('bye', socket.nickname));
+  });
+  socket.on('disconnect', () => {
+    wsServer.sockets.emit('room_change', publicRooms());
   });
   socket.on('new_message', (msg, room, done) => {
     socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`);
